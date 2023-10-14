5364fa04fa5e24a9c015d1c9a811248a1b2b6cb7
GoodMafia <danya.makagonov@internet.ru>
2023-10-14 16:43:05 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './Css/App.css';
 import Third from './Components/Third/Third';
+import { buildQueries } from '@testing-library/react';
+
+const blocks = [{speed: 10, price: 300, color: 'blue'}, {speed: 50, price: 450, color: 'green'}, {speed: 100, price: 550, color: 'red'}, {spped: 200, price: 1000, color: 'black'}];
 
 function App() {
   return (
     <div className="App">
-        <Third speed="10" price="300" color="blue"/>
-        <Third speed="50" price="450" color="green"/>
-        <Third speed="100" price="550" color="red"/>
-        <Third speed="200" price="1000" color="black"/>
+        <Third speed={blocks[0].speed} price={blocks[0].price} color={blocks[0].color}/>
+        <Third speed={blocks[1].speed} price={blocks[1].price} color={blocks[1].color}/>
+        <Third speed={blocks[2].speed} price={blocks[2].price} color={blocks[2].color}/>
+        <Third speed={blocks[3].speed} price={blocks[3].price} color={blocks[3].color}/>
     </div>
   );
 }
